316e4d3d6983bb1b566a7e03f18d325b9d69c536
Zi Nean Teoh <zinean00@gmail.com>
2022-01-29 16:14:02 -0600

    file now automatically generate trajectory graphs for any cars

--- graph_trajectory.py
+++ graph_trajectory.py
@@ -10,6 +10,7 @@ Created on Fri Jan 28 11:15:36 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 def get_derivative(function, time):
     '''
@@ -31,58 +32,72 @@ def get_derivative(function, time):
         t_prime = np.append(t_prime, t_temp)
     return f_prime, t_prime
 
-def plot_figure(computed_plot_info, actual_plot_info, xlabel, ylabel, alpha = 1.0):
+def plot_figure(car_id, 
+                computed_plot_info, 
+                actual_plot_info, 
+                xlabel, 
+                ylabel, 
+                alpha,
+                output_file_name = "figure.png"):
     plt.figure()
     # graph actual (if provided)
     if actual_plot_info != {}:
         plt.plot(actual_plot_info["x_list"], 
                  actual_plot_info["y_list"], 
-                 actual_plot_info["format"],
+                 color = actual_plot_info["color"],
                  alpha = alpha,
                  label = actual_plot_info["label"],
                  markersize = 0.5)
     # graph computed
-    plt.plot(computed_plot_info["x_list"],
-             computed_plot_info["y_list"],
-             computed_plot_info["format"],
-             alpha = alpha,
-             label = computed_plot_info["label"],
-             markersize = 0.5)
-    
+    if computed_plot_info != {}:
+        plt.plot(computed_plot_info["x_list"],
+                 computed_plot_info["y_list"],
+                 color = computed_plot_info["color"],
+                 linestyle = "-",
+                 alpha = alpha,
+                 label = computed_plot_info["label"],
+                 markersize = 0.5)
+    plt.title("Car_id: " + str(car_id))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend()
+    plt.savefig(os.path.join(main_folder, output_file_name))
+
+main_folder = r"C:\Users\teohz\Desktop\smoothness-visualizer"
+gt_path = main_folder + "\TM_10000_GT.csv"
 
 if __name__ == "__main__":
     # read file
-    main_folder = r"C:\Users\teohz\Desktop\smoothness-visualizer"
-    gt_path = main_folder + "\TM_10000_GT.csv"
-    df = pd.read_csv(gt_path)
+    df_orig = pd.read_csv(gt_path)
     
-    # get one specific car
-    df = df[df['ID'] == 5].reset_index(drop=True)
-    x = df['x']
-    time_x = df['Timestamp']
-    
-    # position
-    computed_x = {"x_list": time_x, "y_list": x, "format": "tab:olive", "label": "Computed Position"}
-    plot_figure(computed_x, {}, "time (seconds)", "position, x")
-    
-    # velocity
-    v, time_v = get_derivative(x, time_x)
-    computed_v = {"x_list": time_v, "y_list": v, "format": "tab:green", "label": "Computed Velocity"}
-    actual_v = {"x_list": time_x, "y_list": -df['speed'], "format": "tab:olive", "label": "Actual Velocity"}
-    plot_figure(computed_v, actual_v, "time (seconds)", "velocity, dx/dt", 0.9)
-    
-    # acceleration
-    a, time_a = get_derivative(v, time_v)
-    computed_a = {"x_list": time_a, "y_list": a, "format": "tab:blue", "label": "Computed Accleration"}
-    actual_a = {"x_list": time_x, "y_list": -df['acceleration'], "format": "tab:olive", "label": "Actual Acceleration"}
-    plot_figure(computed_a, actual_a, "time (seconds)", "acceleration, dv/dt", 0.9)
-    
-    # jerk
-    j, time_j = get_derivative(a, time_a)
-    computed_j =  {"x_list": time_j, "y_list": j, "format": "tab:red", "label": "Computed Jerk"}
-    plot_figure(computed_j, {}, "time (seconds)", "jerk, da/dt")
+    for car_id in range(1, 7):
+        df = df_orig[df_orig['ID'] == car_id].reset_index(drop=True)
+        x = df['x']
+        time_x = df['Timestamp']
+        
+        # position
+        output_file_name = "car" + str(car_id) + "_1_position.png"
+        computed_x = {"x_list": time_x, "y_list": x, "color": "tab:olive", "label": "Actual Position"}
+        plot_figure(car_id, computed_x, {}, "time (seconds)", "position, x", 1, output_file_name)
+        
+        # velocity
+        output_file_name = "car" + str(car_id) + "_2_velocity.png"
+        v, time_v = get_derivative(x, time_x)
+        computed_v = {"x_list": time_v, "y_list": -v, "color": "tab:green", "label": "Computed Velocity"}
+        actual_v = {"x_list": time_x, "y_list": df['speed'], "color": "tab:olive", "label": "Actual Velocity"}
+        plot_figure(car_id, computed_v, actual_v, "time (seconds)", "velocity, dx/dt", 0.9, output_file_name)
+        
+        # acceleration
+        output_file_name = "car" + str(car_id) + "_3_acceleration.png"
+        a, time_a = get_derivative(v, time_v)
+        computed_a = {"x_list": time_a, "y_list": -a, "color": "tab:blue", "label": "Computed Accleration"}
+        actual_a = {"x_list": time_x, "y_list": df['acceleration'], "color": "tab:olive", "label": "Actual Acceleration"}
+        plot_figure(car_id, computed_a, actual_a, "time (seconds)", "acceleration, dv/dt", 0.9, output_file_name)
+        
+        # jerk
+        output_file_name = "car" + str(car_id) + "_4_jerk.png"
+        j, time_j = get_derivative(a, time_a)
+        computed_j =  {"x_list": time_j, "y_list": j, "color": "tab:red", "label": "Computed Jerk"}
+        plot_figure(car_id, computed_j, {}, "time (seconds)", "jerk, da/dt", 1, output_file_name)
 
     
